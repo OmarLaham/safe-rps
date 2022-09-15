43c66cabc2abd7581bc67158b43390c540df1721
OmarLaham <omla00001@stud.uni-saarland.de>
2022-09-15 11:14:15 +0200

    update create-pipeline page

--- assets/js/create-pipeline.js
+++ assets/js/create-pipeline.js
@@ -4,3 +4,8 @@ $(document).ready(function() {
     const popoverTriggerList = document.querySelectorAll('[data-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 });
+
+$('.btn-step-del').click(function() {
+    //find pipeline-step-card
+    $(this).closest('.pipeline-step-card').remove();
+});
