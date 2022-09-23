5802ee1abaa5f15ea7e442d4e0fa2ccdd1a64dba
OmarLaham <omla00001@stud.uni-saarland.de>
2022-09-23 12:00:10 +0200

    complete follow by extension feature in create-pipeline page

--- assets/js/create-pipeline.js
+++ assets/js/create-pipeline.js
@@ -34,25 +34,93 @@ $(document).on('click', '.btn-step-del', function() {
 //========================== Adding extensions =========================
 
 //list of exts. added to current pipeline
-var ext_pipe = []
+var ext_pipe = ["upload-data", "preprocessing", "dimensionality-reduction", "clustering-and-finding-markers"]
 
 //dict of exts. available to add (could have already been added or not)
 var ext_available = {
-    "Extension A": {
+    "trajectory-analysis": {
+        "title": "Trajectory Analysis",
         "added": false,
-        "dependencies": ["clustering-and-finding-markers"]
+        "inputs": ['DEA results'],
+        "dependencies": ["clustering-and-finding-markers"],
+        "summary": "<ul><li>Root cells automatically detected</li><li>Pseudotime trajectory analysis</li></ul>",
+        "outputs": ["Trajectory analysis UMAP/tSNE with nodes and pseudotime"]
+    },
+    "go-enrichment-analysis": {
+        "title": "GO Enrichment Analysis",
+        "added": false,
+        "inputs": ['DEA results'],
+        "dependencies": ["clustering-and-finding-markers"],
+        "summary": "<ul><li>EnrichR is used for this analysis</li><li>Enrichemnt and depletion</li><li>Biological Processes</li><li>Cellular Components</li><li>Molecular Functions</li></ul>",
+        "outputs": ["Enrichment analysis results", "Volcano and MA plots for each cluster"]
+    },
+    "extension-x": {
+        "title": "Extension X",
+        "added": false,
+        "inputs": ['DEA results'],
+        "dependencies": ["clustering-and-finding-markers"],
+        "summary": "<ul><li>Example dependcy extension for cell-cell communication extension</li></ul>",
+        "outputs": [".csv file for.."]
+    },
+    "cell-cell-communication": {
+        "title": "Cell-Cell Communication",
+        "added": false,
+        "inputs": ['DEA results'],
+        "dependencies": ["clustering-and-finding-markers", "Extension X"],
+        "summary": "<ul><li>Root cells automatically detected</li><li>Pseudotime trajectory analysis</li></ul>",
+        "outputs": ["Aggregated cell-cell communication network", "Single and overall visualization for the aggregated cell-cell communication network"]
     }
 }
 
 var ext_add_after = "";
 
+//on add extension modal firing
 $('#modal-add-extension').on('shown.bs.modal', function () {
+
+    //alter available ext. list in add extension modal
+    var html = ""; //html to show in modal-ext-lst inside modal
+
+    //list all available NOT added extensions
+    Object.keys(ext_available).forEach(function(key) {
+        //console.log(key, ext_available[key]);
+        let ext = ext_available[key];
+
+        if (ext["added"] === false) {//list only non-added ext.
+            let dependencies = ext["dependencies"];
+            var missing_dependencies = [];
+            for (let i = 0; i < dependencies.length; i++) {
+                if(ext_pipe.includes(dependencies[i]) === false) {
+                    missing_dependencies.push("<span class='badge bg-light'>" + dependencies[i] + "</span>");
+
+                }
+            }
+            if(missing_dependencies.length == 0) { // no missing dependencies
+                html += "<input type='radio' name='rdo-add-ext-names' value='" + key + "'> " + ext["title"] + "<br />";
+            } else { // some missing dependencies
+                html += "<input type='radio' name='rdo-add-ext-names' disabled='disabled' value='" + key + "'> " + ext["title"] + " <span class='badge bg-danger text-light'>Missing Dependencies: </span>: " + missing_dependencies.join("&nbsp;") + "<br />";
+            }
+        }
+
+
+    });
+
+
+    if(html != "") {
+        $('#btn-add-extension').removeClass('d-none');
+    } else { //if all are already added!
+        html = "No other extensions can be added to your pipeline!<br />We are always happy to <a href='mailto@p.omarlahham@gmail.com'>get in touch</a> to discuss custom extensions for your research or business."
+        $('#btn-add-extension').addClass('d-none');
+    }
+
+    //replace old HTML
+    $('#modal-ext-lst').html(html);
 
 });
 
 $(document).on('click', '.btn-follow-by-extension', function() {
-    //find pipeline-step-card
+    //find pipeline-step-card to add extension after it
     ext_add_after = $(this).closest('.pipeline-step').attr("id");
+
 });
 
 $("#btn-add-extension").click(function() {
@@ -60,13 +128,14 @@ $("#btn-add-extension").click(function() {
     //get extension template html
     var ext_html = $('#extension-template').html();
 
-    let ext_title = "Extension A";
-    let ext_id = "step-" + ext_title.replace(" ", "-").toLowerCase();
+    //get id of added extension
+    let ext_id = $("input[name=rdo-add-ext-names]").val();
 
-    let ext_inputs = ["inp1", "inp2"];
-    let ext_dep = ["dep1", "dep2"];
-    let ext_summary = "<li>summary1</li><li>summary2</li>";
-    let ext_outputs = ["output1", "output2"];
+    let ext_title = ext_available[ext_id]["title"];
+    let ext_inputs = ext_available[ext_id]["inputs"];
+    let ext_dep = ext_available[ext_id]["dependencies"];
+    let ext_summary = ext_available[ext_id]["summary"];
+    let ext_outputs = ext_available[ext_id]["outputs"];
 
     //set card id so we can added it to fill the gaps in the template
     ext_html = ext_html.replace("step-id-place-holder", ext_id);
@@ -88,7 +157,7 @@ $("#btn-add-extension").click(function() {
     //set dependecies
     for (var i = 0; i < ext_dep.length; i++) {
         let html = "<span class=\"badge bg-light step-input\">" + ext_dep[i] + "</span>&nbsp;";
-        $('#' + ext_id + " .step-dependecies").append(html);
+        $('#' + ext_id + " .step-dependencies").append(html);
     }
 
     //set outputs
